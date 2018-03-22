b5033a248091172a9a57eb16c708d8edcf5ec17f
Carles Sanchez Romero <carles.sanchez@atmira.com>
2018-03-22 16:22:23 +0100

    Añade los 3 proyectos a la solución e implementa etodos de test y principales

---
+++ PooVuelingXunitTests/CalculadoraIntegrationTestsXUnit.cs
@@ -0,0 +1,43 @@
+﻿using PooVueling;
+using System;
+using Xunit;
+
+namespace PooVueling.XUnitTests
+{
+    public class CalculadoraIntegrationTests
+    {
+        ICalculadora iCalculadora = new Calculadora();
+        Random rnd = new Random();
+
+        [Fact]
+        public void DivisionTest()
+        {
+            Assert.Equal(2,iCalculadora.Division(4,2));
+        }
+
+        [Fact]
+        public void DivisionBy0Test()
+        {
+            var ex = Assert.Throws<DivideByZeroException>(() => iCalculadora.Division(rnd.Next(1, 10), 0));
+            Assert.IsType<DivideByZeroException>(ex);
+        }
+
+        [Fact]
+        public void MultiplicacionTest()
+        {
+            Assert.Equal(6,iCalculadora.Multiplicacion(2, 3));
+        }
+
+        [Fact]
+        public void RestaTest()
+        {
+            Assert.Equal(0,iCalculadora.Resta(1, 1));
+        }
+
+        [Fact]
+        public void SumaTest()
+        {
+            Assert.Equal(4,iCalculadora.Suma(2, 2));
+        }
+    }
+}
