a6c0afa526ed32d296aac3b342a9b46d3c173d60
Pere Zamora <pere.zamora@atmira@com>
2018-03-28 23:05:09 +0200

    Simple log4net log message

--- PooVueling/Program.cs
+++ PooVueling/Program.cs
@@ -8,9 +8,13 @@ namespace PooVueling
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main (string[] args)
         {
-
+            log.Info("Hello logging world!");
+            Console.WriteLine("Hit enter");
+            Console.ReadLine();
         }
     }
 }
